3ee88de86704020e915e448f7f9f8b740a482b58
João Victor <victorbarboza@outlook.com>
2018-03-28 07:22:37 -0300

    Página de calendário

--- app/src/main/java/pds/ufrn/com/br/agendadoaluno/Calendar.java
+++ app/src/main/java/pds/ufrn/com/br/agendadoaluno/Calendar.java
@@ -1,13 +1,30 @@
 package pds.ufrn.com.br.agendadoaluno;
 
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.CalendarView;
+import android.widget.TextView;
 
 public class Calendar extends AppCompatActivity {
+
+    private TextView textView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_calendar);
+
+        textView = findViewById(R.id.textView);
+        CalendarView calendarView = findViewById(R.id.calendarView);
+
+        calendarView.setOnDateChangeListener(new CalendarView.OnDateChangeListener() {
+            @Override
+            public void onSelectedDayChange(@NonNull CalendarView calendarView, int yyyy, int mm, int dd) {
+                String text = "Data: " + dd + "/" + mm + "/" + yyyy;
+                textView.setText(text);
+            }
+        });
+
     }
 }
